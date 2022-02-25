8f201f5ba913ce94e901004c7760502be44021c3
feelsabound <arielja87.2@gmail.com>
2022-02-25 14:22:54 -0800

    working animations and fading

--- js/codewords.js
+++ js/codewords.js
@@ -1,2 +1,25 @@
-document.getElementById("")
-document.getElementById("code1").innerHTML
+function playWaltz() {
+    var sound = document.getElementById("audio");
+    sound.play();
+    document.body.style.animationPlayState = "running";
+    document.getElementById("moon").style.animationPlayState = "running";
+    document.getElementById("moonflip").style.animationPlayState = "running";
+    document.getElementById("sun").style.animationPlayState = "running";
+}
+
+document.getElementById("submit").onclick = submitAction;
+
+function submitAction(event)
+{
+    event.preventDefault();
+    console.log(document.getElementById("code1").value)
+
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("code1", "Smith");
+      } else {
+        // Sorry! No Web Storage support..
+      }
+    return false
+}
+
+
