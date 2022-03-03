dec8089fd7afee5749c494f646504d8f3f9474bb
Josh Ariel <josh.ariel@asml.com>
2022-03-03 11:51:02 -0800

    disallow mobile

---
+++ js/checkmobile.js
@@ -0,0 +1,6 @@
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.write(
+        <html><p>This page only works on a browser, preferable Chrome, with sound on ;)</p></html>
+    )
+    document.write('<!--');
+  }
